c987515062ab6b283ee6a1607645de3994e6239b
Siddhanth Manish Srivastava <128384938+Sidman0709@users.noreply.github.com>
2025-06-27 20:28:44 +0530

    backnd started

---
+++ backend/extention/src/main/java/com/cyberbullying/extention/ExtentionApplication.java
@@ -0,0 +1,13 @@
+package com.cyberbullying.extention;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExtentionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExtentionApplication.class, args);
+	}
+
+}
---
+++ backend/extention/src/test/java/com/cyberbullying/extention/ExtentionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cyberbullying.extention;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExtentionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
